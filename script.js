33874f45d39ea0e9748d415203c2428f44b1e06d
tonsz <tmestrera@gmail.com>
2025-02-05 17:23:30 +0800

    colors persist

--- script.js
+++ script.js
@@ -2,7 +2,55 @@ $(document).ready(function () {
   let cols = $(".container").width() / 20;
   let rows = $(".container").height() / 20;
 
+  let colorClasses = ["lime", "apple", "mustard", "water"];
+  let activity = "click";
+  let curColorClass = "lime";
+
+  // grid setup
   for (var count = 0; count <= cols * rows - 1; count++) {
-    $(".container").append(`<div class="box"></div>`);
+    $(".container").append(`<div class="box" id="b-${count}"></div>`);
   }
+  // hover effect
+  // $(".box")
+  //   .on("mouseenter", function (event) {
+  //     $(`#${event.target.id}`).addClass(curColorClass);
+  //   })
+  //   .on("mouseleave", function (event) {
+  //     let boxId = `#${event.target.id}`;
+  //     if (!$(boxId).hasClass("ticked")) {
+  //       $(boxId).removeClass(curColorClass);
+  //     }
+  //   });
+
+  function changeColor() {}
+
+  function paint(e) {
+    let boxId = `#${e.target.id}`;
+    $(boxId).addClass("ticked");
+    $(boxId).addClass(curColorClass);
+  }
+
+  $(".box").on(activity, paint);
+
+  // toggle between hover and click
+  $(document).keypress(function (event) {
+    if (event.key === "h") {
+      if (activity === "click") {
+        activity = "mouseover";
+        $(".box").on("mouseover", paint);
+        $(".box").off("click", paint);
+      } else {
+        activity = "click";
+        $(".box").off("mouseover", paint);
+        $(".box").on("click", paint);
+      }
+    }
+  });
+
+  // color picker
+  $(".color").on("click", function (event) {
+    $(".color").removeClass("picked");
+    $(`#${event.target.id}`).addClass("picked");
+    curColorClass = event.target.id;
+  });
 });
