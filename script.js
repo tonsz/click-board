e769b2627fda987e8880798810f76d9f8b820f11
tonsz <tmestrera@gmail.com>
2025-02-07 21:30:40 +0800

    working out hover, still very buggy

--- script.js
+++ script.js
@@ -5,29 +5,46 @@ $(document).ready(function () {
   let colorClasses = ["lime", "apple", "mustard", "water"];
   let activity = "click";
   let curColorClass = "lime";
+  let prevClass = "";
 
   // grid setup
   for (var count = 0; count <= cols * rows - 1; count++) {
     $(".container").append(`<div class="box" id="b-${count}"></div>`);
   }
   // hover effect
-  // $(".box")
-  //   .on("mouseenter", function (event) {
-  //     $(`#${event.target.id}`).addClass(curColorClass);
-  //   })
-  //   .on("mouseleave", function (event) {
-  //     let boxId = `#${event.target.id}`;
-  //     if (!$(boxId).hasClass("ticked")) {
-  //       $(boxId).removeClass(curColorClass);
-  //     }
-  //   });
 
-  function changeColor() {}
+  $(".box")
+    .on("mouseenter", function (event) {
+      if ($(`#${event.target.id}`).hasClass("ticked")) {
+        prevClass = this.className;
+      }
+      changeColor(`#${event.target.id}`);
+    })
+    .on("mouseleave", function (event) {
+      if ($(`#${event.target.id}`).hasClass("ticked")) {
+        // prevClass = "";
+      } else {
+        $(`#${event.target.id}`).removeClass();
+        $(`#${event.target.id}`).addClass("box");
+
+        $(`#${event.target.id}`).addClass(prevClass); // something wrong here, box is being included lol
+      }
+      // let boxId = `#${event.target.id}`;
+      // if (!$(boxId).hasClass("ticked")) {
+      //   $(boxId).removeClass(curColorClass);
+      // }
+    });
+
+  function changeColor(id) {
+    $(id).removeClass();
+    $(id).addClass("box");
+    $(id).addClass(curColorClass);
+  }
 
   function paint(e) {
     let boxId = `#${e.target.id}`;
+    changeColor(boxId);
     $(boxId).addClass("ticked");
-    $(boxId).addClass(curColorClass);
   }
 
   $(".box").on(activity, paint);
